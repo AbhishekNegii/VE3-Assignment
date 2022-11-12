7e54fef6f4ff83506229057770f68be4523e62d0
Abhisheknegii <abhibaba007@gmail.com>
2022-11-12 19:00:44 +0530

    Search functionality  added

--- src/Search/SearchPage.js
+++ src/Search/SearchPage.js
@@ -1,74 +1,58 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import HomeButton from "../HomeButton";
 import "./SearchPage.css";
 import "../MainPage.css";
+import axios from "axios";
 
 const SearchPage = () => {
-  const [data, setData] = useState([]);
-  // const names = ['James', 'John', 'Paul', 'Ringo', 'George'];
-  const countries = [
-    { name: "Belgium", continent: "Europe" },
-    { name: "India", continent: "Asia" },
-    { name: "Bolivia", continent: "South America" },
-    { name: "Ghana", continent: "Africa" },
-    { name: "Japan", continent: "Asia" },
-    { name: "Canada", continent: "North America" },
-    { name: "New Zealand", continent: "Australasia" },
-    { name: "Italy", continent: "Europe" },
-    { name: "South Africa", continent: "Africa" },
-    { name: "China", continent: "Asia" },
-    { name: "Paraguay", continent: "South America" },
-    { name: "Usa", continent: "North America" },
-    { name: "France", continent: "Europe" },
-    { name: "Botswana", continent: "Africa" },
-    { name: "Spain", continent: "Europe" },
-    { name: "Senegal", continent: "Africa" },
-    { name: "Brazil", continent: "South America" },
-    { name: "Denmark", continent: "Europe" },
-    { name: "Mexico", continent: "South America" },
-    { name: "Australia", continent: "Australasia" },
-    { name: "Tanzania", continent: "Africa" },
-    { name: "Bangladesh", continent: "Asia" },
-    { name: "Portugal", continent: "Europe" },
-    { name: "Pakistan", continent: "Asia" },
-  ];
-  const searchHandler = (e) => {
-    e.preventDefault();
-    setData(e.target.value);
-    console.log("getData", e.target.value);
-  };
+  const [posts, setPosts] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+
+
+  useEffect(() => {
+    let arr=[]
+    const loadPosts = async () => {
+       const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      for(let i=0;i<20;i++){
+        arr.push(response.data[i])
+      }
+      setPosts(arr)
+      
+      console.log(response.data.length)
+      
+    };
+
+    loadPosts();
+  }, []);
+
   return (
     <div className="containerSearch">
       <div className="home">
-        <HomeButton />
+      <HomeButton/>
       </div>
-      <div className="textcentre">
-        <h4>Type to search</h4>
-        <br />
-        <input
-          type="text"
-          placeholder="type text & press enter..."
-          size="50"
-          value={data}
-          onChange={searchHandler}
-        />
-        <br />
-        <ul>
-        {/* {countries.map((item)=>(
-          <li>
-           <h4>Country </h4>: {item.name}  <h4>Continent </h4>:{item.continent}
-          </li>
-        ))} */}
-        {countries.filter(name => name.includes(data)).map(filteredName => (
-        <li>
-          {filteredName}
-        </li>
-      ))}
-        </ul>
-        {!data && <p>Showing 0 results</p>}
-      </div>
+      <h4>Type here to search....</h4>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setSearchTitle(e.target.value)}
+      />
+      {posts
+          .filter((value) => {
+            if (searchTitle === "") {
+              return null;
+            } else if (
+              value.title.toLowerCase().includes(searchTitle.toLowerCase())
+            ) {
+              return value;
+            }
+          })
+          .map((item) => <h5 key={item.id}><>{item.title}</></h5>)
+      }
+      {posts && <h5>No result found</h5>}
     </div>
   );
-};
-
+}
 export default SearchPage;
+
